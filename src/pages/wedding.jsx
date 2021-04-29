5697765fb998aad699bf9cdb7ebe489eb2b23852
Mike Goolsby <mgoolsby1091@gmail.com>
2021-04-29 14:36:26 -0400

    stylizes header font, adds ubuntu fn

--- src/pages/wedding.jsx
+++ src/pages/wedding.jsx
@@ -25,7 +25,11 @@ const Wedding = () => {
       className={classes.root}
       >
           <h1 className="title">Welcome, Friends & Family</h1>
-          <h5>You'll see more here soon!</h5>
+          <h5>We're so happy to celebrate our wedding with you all!</h5>
+          {/* <img className="splash" 
+          src="https://pro2-bar-s3-cdn-cf1.myportfolio.com/136d0e9c-3497-402a-ad61-09e7b5c268a4/298ccaaf-a143-4775-b211-b2d4e4d1e363_rw_1920.jpg?h=2329d5c4d072941dc40b9f7b2572d89c"
+          alt="image of bride and groom and their dog"></img> */}
+
       </Grid>
 
 
